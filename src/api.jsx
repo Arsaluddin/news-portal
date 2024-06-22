19b57d9ff6ca9392e2632d47af56ce1b216f9f55
arsal uddin <arsaluddin134@gmail.com>
2024-06-22 10:30:27 +0530

    env

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '769d5b00f0ee4ba4a1ea3b01a509b203';
+const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'https://newsapi.org/v2/everything';
 
 const getYesterdayDate = () => {
@@ -21,9 +21,6 @@ export const fetchArticles = async (query = 'india', category = '', page = 1) =>
         language: 'en',
         sortBy: 'publishedAt',
       },
-      headers: {
-        'X-Api-Key': API_KEY,
-      },
     });
     return response.data;
   } catch (error) {
@@ -35,8 +32,8 @@ export const fetchArticles = async (query = 'india', category = '', page = 1) =>
 export const fetchArticleByUrl = async (url) => {
   try {
     const response = await axios.get(url, {
-      headers: {
-        'X-Api-Key': API_KEY,
+      params: {
+        apiKey: API_KEY,
       },
     });
     return response.data;
@@ -45,6 +42,7 @@ export const fetchArticleByUrl = async (url) => {
     throw error;
   }
 };
+
 
 
 
